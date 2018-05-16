a7abdd41cea61e685d92b17c955143356b0d4206
derek <derek.l.smith@protonmail.com>
2018-05-16 19:30:15 -0400

    C

--- up.sh
+++ up.sh
@@ -4,6 +4,12 @@
 # since all file references are relative to this script
 cd "$(dirname "$0")"
 
+source ./.env
+
+lxc config set core.proxy_http $HTTP_PROXY
+lxc config set core.proxy_https $HTTPS_PROXY
+lxc config set core.proxy_ignore_hosts image-server.local
+
 echo "creating an LXD system container template for running docker applications."
 ./host_template/up.sh
 
