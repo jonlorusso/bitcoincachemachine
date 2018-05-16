403c12ed92c8383f23a1b236688c4788673b8939
derek <derek.l.smith@protonmail.com>
2018-05-16 13:39:28 -0400

    C

--- up.sh
+++ up.sh
@@ -10,8 +10,8 @@ echo "creating an LXD system container template for running docker applications.
 echo "Deploying proxyhost"
 ./proxyhost/up.sh
 
-# echo "Creating swarm with 3 managers"
-# ./managers/up.sh
+echo "Creating swarm with 3 managers"
+./managers/up.sh
 
 # echo "deploying an lxd host for nextcloud"
 # ./nextcloud/up.sh
