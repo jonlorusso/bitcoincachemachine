9da23be92e04d599ee821fb502fd14f674783790
derek <derek.l.smith@protonmail.com>
2018-05-18 20:04:46 -0400

    C

--- up.sh
+++ up.sh
@@ -21,9 +21,9 @@ echo "creating an LXD system container template for running docker applications.
 echo "Deploying proxyhost"
 ./proxyhost/up.sh
 
-# echo "Creating swarm with 3 managers"
-# ./managers/up.sh
+echo "Creating swarm with 3 managers"
+./managers/up.sh
 
-# echo "deploying an bitcoin infrastructure."
-# ./bitcoin/up.sh
+echo "deploying an bitcoin infrastructure."
+./bitcoin/up.sh
 
